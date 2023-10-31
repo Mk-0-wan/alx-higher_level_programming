0af769169f9feb383e27fc8df24b9937e7d0d920
archMk <victormurithi2020@gmail.com>
2023-10-31 16:42:42 +0300

    python reverse alphabets

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-#index to keep track of each character position
-#start at index 1
+# index to keep track of each character position
+# start at index 1
 index = 1
 
-#enumerate over each character and collect its index
+# enumerate over each character and collect its index
 for i, char in enumerate(range(122, 96, -1)):
-    #perform an uppercase logic to that char
-    #increment index by 2 to have an alternate sequence
+    # perform an uppercase logic to that char
+    # increment index by 2 to have an alternate sequence
     if i == index:
         char -= 32
         index += 2
