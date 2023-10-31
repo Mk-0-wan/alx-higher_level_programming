be4a5ba7c7748c1f55f6dbbc5b1a74cad78c868b
archMk <victormurithi2020@gmail.com>
2023-10-31 09:55:38 +0300

    dissasembler

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import dis
 
-def magic_function(a, b):
-    return 98 + (a ** b)
+def magic_calculation(a, b):
+    return (98 + a**b)
 
-print(dis.dis(magic_function))
+print(dis.dis(magic_calculation))
