c7a8defc642f601bf4ede583a84e2308aa5be96f
archMk <victormurithi2020@gmail.com>
2023-11-01 08:44:31 +0300

    printing combo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,5 @@
 for i in range(10):
     for j in range(10):
         if i < j:
-            print("{}{}".format(i, j), end='\n' if (i == 8 and j == 9) else ', ')
+            print("{}\
+{}".format(i, j), end='\n' if (i == 8 and j == 9) else ', ')
