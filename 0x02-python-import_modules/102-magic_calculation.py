d3c84e6a60c69600b7c4cb855382c3ce46411b15
Victor Mk <114664618+Mk-0-wan@users.noreply.github.com>
2023-11-03 00:58:55 +0300

    Update 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-from sm_math_operation import add, sub
-if __name__ == "__main__":
-    def magic_calculation(a, b):
-        if (a < b):
-            c = add(a, b)
-            for i in range(4, 6):
-                c = add(i, c)
-                return (c)
-        else:
-            return (sub(a, b))
+def magic_calculation(a, b):
+    from sm_math_operation import add, sub
+    if (a < b):
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(i, c)
+            return (c)
+    else:
+        return (sub(a, b))
