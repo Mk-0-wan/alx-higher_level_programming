1115b6fe2fff49b2e1165f1436624da7f7345b24
archMk <victormurithi2020@gmail.com>
2023-11-02 12:26:53 +0300

    print alphabets

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-call = lambda start, stop: print(*list(map(chr, range(start, stop))), sep="", end="\n")
-call(65, 91)
+numbers = list(range(65, 91))
+print(*list(map(chr, numbers)), sep="", end="\n")
