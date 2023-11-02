b20fddb4597d348ab5f246e41b80e642335d8515
archMk <victormurithi2020@gmail.com>
2023-11-02 14:50:29 +0300

    my own python calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-
 from sys import argv
 from calculator_1 import add, sub, mul, div
+if __name__ != "__main__":
+    exit()
 
-# handle the number of argument passed
-# failed program should exit with status of 1
-# cast all the argument into integers
-# output should be like this <a> <operators> <b> = <results>
 if (len(argv) - 1) != 3:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>", end='\n')
+    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
     exit(1)
 # handle the operators
 elif argv[2] == "+":
@@ -21,10 +18,11 @@ elif argv[2] == "/":
     dynamic_func = div
 # handle non valid operators
 else:
-    print("Unknown operator. Available operators: +, -, * and /\n")
+    print("Unknown operator. Available operators: +, -, * and /")
     exit(1)
-
+# handle the number of argument passed
+# failed program should exit with status of 1
+# cast all the argument into integers
+# output should be like this <a> <operators> <b> = <results>
 x = dynamic_func(int(argv[1]), int(argv[3]))
 print("{:d} {:s} {:d} = {:d}".format(int(argv[1]), argv[2], int(argv[3]), x))
-if __name__ != "__main__":
-    exit()
