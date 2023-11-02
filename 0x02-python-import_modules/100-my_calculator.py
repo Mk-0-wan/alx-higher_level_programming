16bba7513479b8ec3b81b4523f2080e732eb55f3
archMk <victormurithi2020@gmail.com>
2023-11-02 14:51:56 +0300

    my own python calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 from sys import argv
 from calculator_1 import add, sub, mul, div
-if __name__ != "__main__":
-    exit()
 
 if (len(argv) - 1) != 3:
     print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
@@ -26,3 +24,5 @@ else:
 # output should be like this <a> <operators> <b> = <results>
 x = dynamic_func(int(argv[1]), int(argv[3]))
 print("{:d} {:s} {:d} = {:d}".format(int(argv[1]), argv[2], int(argv[3]), x))
+if __name__ != "__main__":
+    exit()
