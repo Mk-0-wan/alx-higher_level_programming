c5ef59fb95c817c72bc8c6ea161a25ab03f86c6a
Victor Mk <114664618+Mk-0-wan@users.noreply.github.com>
2023-11-03 00:59:24 +0300

    Update 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -5,6 +5,6 @@ def magic_calculation(a, b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(i, c)
-            return (c)
+        return (c)
     else:
         return (sub(a, b))
