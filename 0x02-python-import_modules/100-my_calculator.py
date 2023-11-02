c67b02f19dd5078122ad4540ba335809d046d092
archMk <victormurithi2020@gmail.com>
2023-11-02 14:31:10 +0300

    my own python calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,36 +3,34 @@
 from sys import argv
 from calculator_1 import add, sub, mul, div
 
-res = dynamic_func(int(argv[1]), int(argv[3]))
-
 # handle the number of argument passed
 # failed program should exit with status of 1
 # cast all the argument into integers
 # output should be like this <a> <operators> <b> = <results>
 if (len(argv) - 1) != 3:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>", end='\n')
-    sys.exit(1)
+    exit(1)
 # handle the operators
 elif argv[2] == "+":
     res = add(int(sys.argv[1]), int(sys.argv[3]))
-    print(f"{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {res}")
-    sys.exit(0)
+    print(f"{argv[1]} {argv[2]} {argv[3]} = {res}")
+    exit(0)
 elif argv[2] == "-":
-    res = sub(int(sys.argv[1]), int(sys.argv[3]))
-    print(f"{sys.argv[1]:d} {sys.argv[2]:s} {sys.argv[3]:d} = {res:d}")
-    sys.exit(0)
+    res = sub(int(argv[1]), int(argv[3]))
+    print(f"{argv[1]} {argv[2]} {argv[3]} = {res}")
+    exit(0)
 elif argv[2] == "*":
     res = mul(int(sys.argv[1]), int(sys.argv[3]))
-    print(f"{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {res}")
-    sys.exit(0)
+    print(f"{argv[1]} {argv[2]} {argv[3]} = {res}")
+    exit(0)
 elif argv[2] == "/":
     res = div(int(sys.argv[1]), int(sys.argv[3]))
-    print(f"{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {res}")
-    sys.exit(0)
+    print(f"{argv[1]} {argv[2]} {argv[3]} = {res}")
+    exit(0)
 # handle non valid operators
 else:
     print("Unknown operator. Available operators: +, -, * and /\n")
-    sys.exit(1)
+    exit(1)
 
 if __name__ == "__main__":
     exit()
