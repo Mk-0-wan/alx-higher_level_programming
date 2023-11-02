5e3a33540a6837ea0e4f0c8f64ae8d8a4274209f
archMk <victormurithi2020@gmail.com>
2023-11-03 01:07:19 +0300

    fixing checker issues

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
     from sm_math_operation import add, sub
     if (a < b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(i, c)
-        return (c)
+            return (c)
     else:
         return (sub(a, b))
