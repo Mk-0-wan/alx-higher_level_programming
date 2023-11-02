236114b9707c5a8cd315159dc26108c0ff2fc843
archMk <victormurithi2020@gmail.com>
2023-11-02 22:55:01 +0300

    extracting from .pyc file

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import hidden_4
+
+if __name__ == "__main__":
+    for methods in dir(hidden_4):
+        if (methods == str.startwith(__)):
+            continue
+        else:
+            print(methods)
