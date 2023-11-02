6c594f6029111a1f8f44375663b6254819d8b92d
archMk <victormurithi2020@gmail.com>
2023-11-02 14:18:12 +0300

    my own python calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
 
-import sys
-import calculator_1
+from sys import argv
+from calculator_1 import add, sub, mul, div
 
 # handle the number of argument passed
 # failed program should exit with status of 1
 # cast all the argument into integers
 # output should be like this <a> <operators> <b> = <results>
-if (len(sys.argv) - 1) != 3:
+if (len(argv) - 1) != 3:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>", end='\n')
     sys.exit(1)
 # handle the operators
-elif sys.argv[2] == "+":
-    res = calculator_1.add(int(sys.argv[1]), int(sys.argv[3]))
+elif argv[2] == "+":
+    res = add(int(sys.argv[1]), int(sys.argv[3]))
     print(f"{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {res}")
     sys.exit(0)
-elif sys.argv[2] == "-":
-    res = calculator_1.sub(int(sys.argv[1]), int(sys.argv[3]))
+elif argv[2] == "-":
+    res = sub(int(sys.argv[1]), int(sys.argv[3]))
     print(f"{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {res}")
     sys.exit(0)
-elif sys.argv[2] == "*":
-    res = calculator_1.mul(int(sys.argv[1]), int(sys.argv[3]))
+elif argv[2] == "*":
+    res = mul(int(sys.argv[1]), int(sys.argv[3]))
     print(f"{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {res}")
     sys.exit(0)
-elif sys.argv[2] == "/":
-    res = calculator_1.div(int(sys.argv[1]), int(sys.argv[3]))
+elif argv[2] == "/":
+    res = div(int(sys.argv[1]), int(sys.argv[3]))
     print(f"{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {res}")
     sys.exit(0)
 # handle non valid operators
