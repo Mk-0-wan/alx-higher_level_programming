39e4a0cb49402797d40c9cadf988b8c9a1b46027
archMk <victormurithi2020@gmail.com>
2023-11-02 11:49:07 +0300

    another coool magic calculation

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sm_math_operation import add, sub
+
+def magic_calculation(a, b):
+    if (a < b):
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(i, c)
+        return (c)
+    else:
+        return (sub(a, b))
