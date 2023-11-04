c834c4dd10fec159c0da1a58fa9010f851218464
archMk <victormurithi2020@gmail.com>
2023-11-04 20:43:48 +0300

    it's or

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
-    x = len(my_list)
-    if idx < 0 and idx > x-1:
+    x = len(new_list)
+    if idx < 0 or idx > x-1:
         return new_list
     else:
         new_list[idx] = int(element)
