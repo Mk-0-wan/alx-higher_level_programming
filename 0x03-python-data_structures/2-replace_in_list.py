5bfe14f48b6167ff89507ee460d1853c2248aa31
archMk <victormurithi2020@gmail.com>
2023-11-04 17:30:45 +0300

    replacing elements

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# modifying a list element
+# if the indx is out of rang return the original list
+# if the indx is less than 0 return the original list
+def replace_in_list(my_list, idx, element):
+    x = len(my_list)
+    if (x == 0):
+        exit
+    for i in range(0, x):
+        if (i == idx):
+           my_list[i] = element
+           return my_list
+        elif idx < 0 or idx > x:
+            return my_list
