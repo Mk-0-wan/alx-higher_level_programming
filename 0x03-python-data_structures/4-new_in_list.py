3c463785f341ed05685c895ca7efa6cf4e6d56d0
archMk <victormurithi2020@gmail.com>
2023-11-04 20:34:06 +0300

    not beyond or below the range

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     if len(my_list) == 0:
         return None
-    elif idx < 0 and idx > x:
+    elif idx < 0 and idx > x-1:
         return None
     else:
         new_list = my_list.copy()
