a117aae1bc2fa8add8b8f43bd841a333c7e2c446
archMk <victormurithi2020@gmail.com>
2023-11-04 17:39:47 +0300

    fixed indexing cast

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -8,7 +8,7 @@ def replace_in_list(my_list, idx, element):
         exit
     for i in range(0, x):
         if (i == idx):
-            my_list[i] = element
+            my_list[i] = int(element)
             return my_list
         elif idx < 0 or idx > x:
             return my_list
