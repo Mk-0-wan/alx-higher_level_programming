37747994a55a65cd1f76a641a019ab5b18c6d288
archMk <victormurithi2020@gmail.com>
2023-11-04 20:36:07 +0300

    fixed the range

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if len(my_list) == 0:
-        return None
-    elif idx < 0 and idx > x-1:
+    x = len(my_list)
+    if idx < 0 and idx > x-1:
         return None
     else:
         new_list = my_list.copy()
-        new_list[idx] = element
+        new_list[idx] = int(element)
         return new_list
