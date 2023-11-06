69c98a2b0f4c8a497baa6d04bb8e353dc0954889
archMk <victormurithi2020@gmail.com>
2023-11-07 00:29:28 +0300

    found a palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks whether the list is a
+ * palindrome or not (if it mirrors itself)
+ * @head: pointer to the first node in the list
+ * Return: 0 if true 1 if false
+ */
+int is_palindrome(listint_t **head)
+{
+	/**
+	 * make a reversed list first
+	 * and an original list
+	 * now loop
+	 *            s_head == r_head
+	 * 1 -> 2 -> 3 -> 4 <- 4 <- 3 <- 2 <- 1
+	 *
+	 */
+	listint_t *head_node = *head;
+	listint_t *tail_node = *head;
+	listint_t *rev_head = NULL, *prev = NULL;
+	/* for reverse list */
+	if (!*head)
+		return (1);
+	/* start from the last point of the list */
+	while (tail_node)
+	{
+		rev_head = tail_node->next;
+		tail_node->next = prev;
+		prev = head_node;
+		tail_node = rev_head;
+	}
+	tail_node = prev; /* move to the last node in the list */
+	while (head_node != NULL && tail_node != NULL)
+	{
+		if (head_node->n == tail_node->n)
+			/* yes it's a palindrome*/
+			return (1);
+		head_node = head_node->next;
+		tail_node = tail_node->next;
+	}
+	return (0);
+}
