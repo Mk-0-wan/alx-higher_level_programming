0dd28688c2e9a0fd3e29d3202955232044c2e415
archMk <victormurithi2020@gmail.com>
2023-11-07 02:02:36 +0300

    pyobjects

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,28 @@
+#include "Python.h"
+#include <stdlib.h>
+
+/**
+ * print_python_list_info - prints information about a python list object
+ * @p: pointer to the PyObject
+ *
+ * Return: always void.
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	PyListObject *py_list = NULL;
+	ssize_t list_len = 0;
+	ssize_t i = 0;
+	const char *element_type = NULL;
+
+	list_len = PyList_Size(p);
+	py_list = (PyListObject *)p;
+	printf("[*] Size of the Python List = %ld\n", list_len);
+	printf("[*] Allocated = %ld\n", (signed long)(py_list->allocated));
+	while (i < list_len)
+	{
+		ele_type = Py_TYPE(py_list->ob_item[i])->tp_name;
+		printf("Element %ld: %s\n", i, element_type);
+		i++;
+	}
+}
