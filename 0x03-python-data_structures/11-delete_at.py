cf8104df8e6f6dde2f26465f509f20be6a57fcb1
archMk <victormurithi2020@gmail.com>
2023-11-06 13:22:06 +0300

    delete at given index

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,8 +2,7 @@
 def delete_at(my_list=[], idx=0):
     if len(my_list) == 0:
         return None
-    elif 0 > idx or idx > len(my_list)-1:
+    elif 0 > idx or idx => len(my_list):
         return my_list
-    else:
-        del my_list[idx]
+    del my_list[idx]
     return (my_list)
