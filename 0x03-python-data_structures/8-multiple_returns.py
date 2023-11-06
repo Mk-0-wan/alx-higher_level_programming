d348aaa8d9152ca6d46a82098eeb9f25815d7f4e
archMk <victormurithi2020@gmail.com>
2023-11-06 14:32:11 +0300

    adding a doc string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    '''
-    returns the length of the string and the first letter
-    '''
     if len(sentence) == 0:
-        return None
+        return sentence[0] = None
 
     return int(len(sentence)), sentence[0]
