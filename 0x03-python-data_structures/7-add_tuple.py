b08338c725a979ec14529f1a75bc3bd87615e719
archMk <victormurithi2020@gmail.com>
2023-11-06 15:14:50 +0300

    fixed the out of range value

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_b) == 1:
-        tuple_3 = (tuple_a[0]+tuple_b[0], tuple_a[1]+0)
-    elif len(tuple_b) == 0:
-        return tuple_a
-    elif len(tuple_b) > 1:
-        tuple_3 = (tuple_a[0]+tuple_b[0], tuple_a[1]+tuple_b[1])
-    return tuple_3
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+    tuple_c = ()
+    for idx in range(2):
+        pos_1 = tuple_a[idx] if idx < len_a else 0
+        pos_2 = tuple_b[idx] if idx < len_b else 0
+        # at the first iteration make a new tuple
+        if (idx == 0):
+            tuple_c = pos_1 + pos_2
+        else:
+            # add new items to the tuple
+            tuple_c = tuple_c, pos_1 + pos_2
+    return tuple_c
