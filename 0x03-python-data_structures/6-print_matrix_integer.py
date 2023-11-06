fd2cff6d279669f6b51f9d506a3ec20ed63eb745
archMk <victormurithi2020@gmail.com>
2023-11-06 15:20:30 +0300

    printing matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-if __name__ != "__main__"
-    def print_matrix_integer(matrix=[[]]):
-        if matrix == []:
-            print()
-        for row in matrix:
-            for i, col in enumerate(row):
-                print("{:d}".format(int(col)), end=(' ', '\n')[i == len(row) - 1])
+def print_matrix_integer(matrix=[[]]):
+    if matrix == []:
+        print()
+    for row in matrix:
+        for i, col in enumerate(row):
+            print("{:d}".format(int(col)), end=(' ', '\n')[i == len(row) - 1])
