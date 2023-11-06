e4d12de36a0258b70555fed1a380e9d113a25c6f
archMk <victormurithi2020@gmail.com>
2023-11-06 13:52:46 +0300

    import checker

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    last = len(my_list)+1
-    for i in range(1, last):
-        i *= -1
-        print("{:d}".format(int(my_list[i])))
+if __name__ != "__main__":
+    def print_reversed_list_integer(my_list=[]):
+        last = len(my_list)+1
+        for i in range(1, last):
+            i *= -1
+            print("{:d}".format(int(my_list[i])))
