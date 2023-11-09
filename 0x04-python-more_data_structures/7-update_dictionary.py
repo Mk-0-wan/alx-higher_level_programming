72755704857922f39b1a2293dd3354b1904a60e4
Victor Mk <114664618+Mk-0-wan@users.noreply.github.com>
2023-11-09 23:35:42 +0300

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,10 +1,18 @@
 def update_dictionary(a_dictionary, key, value):
-    # Check if key exists in the dictionary
-    if key in a_dictionary:
-        # If key exists, update its value
-        a_dictionary[key] = value
-    else:
-        # If key doesn't exist, add a new key-value pair
-        a_dictionary[key] = value
+    # find a key in the dict
+    lc = list(a_dictionary.keys())
+
+    key_found = False
+
+    for kys in range(len(lc)):
+        # if exist change its value
+        if lc[kys] == key:
+            a_dictionary.__setitem__(lc[kys], value)
+            key_found = True
+
+    # if key not found, make a new dict key-value using update({key: value})
+    if not key_found:
+        a_dictionary.update({key: value})
 
     return a_dictionary
+    
