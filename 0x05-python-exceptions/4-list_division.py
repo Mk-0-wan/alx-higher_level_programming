72add178b884ff42e7e6f4b44a694ddf2280ad89
archMk <victormurithi2020@gmail.com>
2023-11-20 20:08:12 +0300

    multi check

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    if (my_list_1, my_list_2) is not None:
+        index = 0
+        new_list = []
+        for elem in range(0, list_length):
+            try:
+                index = my_l_1[elem] / my_l_2[elem]
+            except (TypeError):
+                print("wrong type")
+            except (ZeroDivisionError, FloatingPointError):
+                index = 0
+                print("division by 0")
+            except (IndexError):
+                print("out of range")
+            finally:
+                new_list.append(index)
+        return (new_list)
