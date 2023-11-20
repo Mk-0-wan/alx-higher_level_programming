a3befcdfc3b7361d4cf55afaba84f5135e97fd39
archMk <victormurithi2020@gmail.com>
2023-11-20 17:29:10 +0300

    fixed errors of printsafe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     if my_list is not None:
         index = 0
-        for elem in my_list:
+        for elem in range(0, x):
             try:
                 print("{}".format(my_list[elem]), end="")
-            except (IndexError):
-                print()
-                return (index)
-            else:
                 index += 1
+            except:
+                continue
+        print()
         return index
