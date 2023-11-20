7f2952332ff3b691c140e8fe2c0016b53107ab6d
archMk <victormurithi2020@gmail.com>
2023-11-20 19:53:14 +0300

    div

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,13 +7,3 @@ def safe_print_division(a, b):
         res = None
     finally:
         print("Inside result: {}".format(res))
-
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
