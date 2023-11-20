1c891cf639943e352d2ecaed4f66572de0aaf0f7
archMk <victormurithi2020@gmail.com>
2023-11-20 22:15:58 +0300

    printing int safely

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_print_integer_err(value):
     if value is not None:
-        import sys
         try:
             print("{:d}".format(value))
             return True
