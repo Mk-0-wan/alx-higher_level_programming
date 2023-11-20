82d177970aeeddfade841d18a32914f427a5602f
archMk <victormurithi2020@gmail.com>
2023-11-20 16:42:01 +0300

    fixed zerodivision error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        res = int(a) / int(b)
-    except (ZeroDivisionError, FloatingPointError, OverflowError):
+        if (b > 0):
+            res = int(a) / int(b)
+        else:
+            raise ZeroDivisionError
+    except (ZeroDivisionError):
         res = None
         print("Inside result: {}".format(res))
         return None
