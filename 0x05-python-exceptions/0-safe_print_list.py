88fd8e276063073da8041b08dfbd8de17ec2495c
archMk <victormurithi2020@gmail.com>
2023-11-20 17:19:24 +0300

    print safe fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
-def recursive_len(seq):
-    if not seq:
-        return (0)
-    else:
-        return (1 + recursive_len(seq[1:]))
-
-
 def safe_print_list(my_list=[], x=0):
-    try:
-        if (x <= recursive_len(my_list)):
-            i = 0
-            while i < x:
-                print("{}".format(my_list[i]), end=("", "\n")[i == x-1])
-                i = i + 1
-            return (i)
-        else:
-            raise (IndexError)
-    except (IndexError):
-        y = recursive_len(my_list)
-        for j in range(0, y):
-            print("{}".format(my_list[j]), end=("", "\n")[j == y-1])
-        return (y)
+    if my_list is not None:
+        index = 0
+        for elem in my_list:
+            try:
+                print("{}".format(my_list[elem]), end="")
+            except (IndexError):
+                print()
+                return (index)
+            else:
+                index += 1
+        return index
