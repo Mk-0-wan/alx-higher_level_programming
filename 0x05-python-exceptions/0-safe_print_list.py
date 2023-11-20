117525f9e1d1dd6b083c292ab3c2be4803a2385b
archMk <victormurithi2020@gmail.com>
2023-11-20 12:59:48 +0300

    printing safe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -12,8 +12,6 @@ def safe_print_list(my_list=[], x=0):
         while i < x:
             print("{}".format(my_list[i]), end=("", "\n")[i >= x-1])
             i = i + 1
-        return (int(i))
+        return (i)
     except (IndexError):
-        y = int(recursive_len(my_list))
-        print()
-        return (y)
+        return (recursive_len(my_list))
