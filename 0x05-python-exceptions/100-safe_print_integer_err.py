aa2765a9cce8a88f27e794ae89ef0f6300be7a46
archMk <victormurithi2020@gmail.com>
2023-11-20 21:36:05 +0300

    error manipulation

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    if value is not None:
+            try:
+                print("{:d}".format(value))
+                return True
+            except (TypeError, ValueError) as err:
+                import sys
+                sys.stderr.write("Exception:{}\n".format(err))
+                return False
