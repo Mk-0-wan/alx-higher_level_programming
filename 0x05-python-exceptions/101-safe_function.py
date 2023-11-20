e6991e6904013c3a38c6a707caee5278b5fa0acb
archMk <victormurithi2020@gmail.com>
2023-11-20 22:14:16 +0300

    print safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    if fct is not None:
+        try:
+            x = fct(*args)
+            return (x)
+        except (ZeroDivisionError) as err:
+            sys.stderr.write("Exception: {}\n".format(err))
+            return None
+        except (IndexError) as err2:
+            sys.stderr.write("Exception: {}\n".format(err2))
+            return None
