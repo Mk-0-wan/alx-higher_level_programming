17bd484fbfb4bdd58ab303b873ae8e815d973c99
archMk <victormurithi2020@gmail.com>
2023-11-21 01:29:47 +0300

    printing safe

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,12 +3,9 @@ import sys
 
 
 def safe_print_integer_err(value):
-    if value is not None:
-        try:
-            print("{:d}".format(value))
-            return True
-        except (TypeError, ValueError) as err:
-            sys.stderr.write("Exception:{}\n".format(err))
-            return False
-    else:
-        return None
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError) as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        return False
