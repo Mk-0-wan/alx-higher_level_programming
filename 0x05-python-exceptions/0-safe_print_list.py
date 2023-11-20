63636b15578349dc06adb7f81d95342d0b47ff37
archMk <victormurithi2020@gmail.com>
2023-11-20 17:30:15 +0300

    fixed errors of printsafe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
             try:
                 print("{}".format(my_list[elem]), end="")
                 index += 1
-            except:
+            except (IndexError):
                 continue
         print()
         return index
