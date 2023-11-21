2d37adbcdb8b0250d9d4e5926b56fe731143511d
archMk <victormurithi2020@gmail.com>
2023-11-21 21:25:57 +0300

    classes intro

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """
+    A simple python class that prints the Square of a
+    python object
+    For now its just a simple demonstration of how it should
+    look like
+    """
+    pass
