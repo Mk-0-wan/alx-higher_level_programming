d431a54b7cbbf40592b717400a282df23ee87bc5
archMk <victormurithi2020@gmail.com>
2023-11-21 21:48:31 +0300

    Doc string fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 """Simple Introduction to Python OPP"""
+
+
 class Square:
-    """A simple python class that prints the Square of a python object"""
+    """A simple python class that prints the Square of a python object
+
+    Simple square class that does nothing at the moment
+
+    """
     pass
