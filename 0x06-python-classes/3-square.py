62e51a6782e3e181f20b626e9fc416b491ce8596
archMk <victormurithi2020@gmail.com>
2023-11-21 22:19:04 +0300

    added a method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Implementation of some methods adding exceptions"""
+
+
+class Square:
+    """A simple python Square class while learning OPP
+
+    Handling some exception errors for the instance variables
+
+    Attributes:
+        size (unknown): param that's going to allow us to make
+        the square calculation.
+
+    """
+    def __init__(self, size=0):
+        """Initializes the instances variables for Square class.
+
+        Args:
+            size (unknown): Param that takes in a value which is later
+            on transformed into a private variable
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Calculates the square value of a given value
+
+        Return:
+            The square value of a given arguments
+        """
+        return self.__size ** 2
