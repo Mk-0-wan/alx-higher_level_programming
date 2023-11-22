97c41b67733828671e9233ccba72fbe04505254d
archMk <victormurithi2020@gmail.com>
2023-11-22 06:27:10 +0300

    fuzz finally

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Simple Magic Class"""
+
+
+class MagicClass:
+    """Magic class bytecode"""
+    radius = 0
+
+    def __init__(self):
+        """initializes the variables instances"""
+        self._MagicClass__radius = 0
+        if type(radius) is not int or type(radius) is not float:
+            raise TypeError("radius must be a number")
+        else:
+            self._MagicClass__radius = radius
+
+    def area(self):
+        """calculates the area"""
+        return self._MagicClass__radius ** 2 * math.pi
+
+    def circumference(self):
+        """calculates the circumference"""
+        return 2 * math.pi * self._MagicClass__radius
