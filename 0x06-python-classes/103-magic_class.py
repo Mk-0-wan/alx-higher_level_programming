669bb3fc24a908b29be7e58c5bfc341f0a78b7d4
Victor Mk <114664618+Mk-0-wan@users.noreply.github.com>
2023-11-22 14:49:18 +0300

    Simple Magic file

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -8,7 +8,7 @@ class MagicClass:
     def __init__(self, radius=None):
         """initializes the variables instances"""
         self._MagicClass__radius = 0
-        if type(radius) is not int or type(radius) is not float:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self._MagicClass__radius = radius
 
