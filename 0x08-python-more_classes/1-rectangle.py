2c7403bb0378645ff6cb2a30e273e00845ed7afb
archMk <victormurithi2020@gmail.com>
2023-11-27 22:02:05 +0300

    adding mods to it

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Simple Introduction to Python Rectangle Class"""
+
+
+class Rectangle:
+    """A simple python class creating a Rectangle class with python"""
+
+    def __init__(self, width=0, height=0):
+        """Initializing all the instances attributes
+
+        Args:
+            width (int): value for the longer side of a rectangle
+            height (int): value for the shorter side of the rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getting the initial width value"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """Overwrites the initial value of width to the new one give
+
+        Args:
+            width (int): an int value greater than 0 which will represent the new longer side of the rectangle
+
+        Return:
+            A private instance attribute width value
+        """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        """Getting the initial width value"""
+        return self.__height
+
+    @height.setter
+    def width(self, height):
+        """Overwrites the initial value of height to the new one give
+
+        Args:
+            height (int): an int value greater than 0 which will represent the new longer side of the rectangle
+
+        Return:
+            A private instance attribute width value
+        """
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif width < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
