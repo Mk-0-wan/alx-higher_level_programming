d2510e5e308bc1ae8393915112240ba8620a0327
archMk <victormurithi2020@gmail.com>
2023-11-28 21:57:18 +0300

    fixed some indetation error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,9 +14,9 @@ def add_integer(a, b=98):
     """
 
     if not isinstance(a, (float, int)):
-        raise TypeError("a must be an integer") 
+        raise TypeError("a must be an integer")
     elif not isinstance(b, (float, int)):
-        raise TypeError("b must be an integer") 
+        raise TypeError("b must be an integer")
 
     res = int(a) + int(b)
 
