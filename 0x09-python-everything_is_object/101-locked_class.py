0f7ee0279a81ef792b93f53797ff5b7e367bad43
archMk <victormurithi2020@gmail.com>
2023-11-29 03:58:04 +0300

    main

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""Locked Class"""
+
 
 class LockedClass:
+    """Locked class
+    Args:
+        slots: slots
+    """
     __slots__ = ['first_name']
 
     def __init__(self):
+        """initialize"""
         pass
