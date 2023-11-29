23a12f12237ec5d98e78d1a9725e4c1652ad923b
archMk <victormurithi2020@gmail.com>
2023-11-30 00:50:42 +0300

    matrix error fixes

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -13,11 +13,11 @@ def matrix_mul(m_a, m_b):
     """
 
     # checking for lists in matrix
-    # if not isinstance(m_a, list):
-        # raise TypeError("m_a must be a list")
-    # if not isinstance(m_b, list):
-        #raise TypeError("m_b must be a list")
-        
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+
     # checking for list of lists in matrix
     if not all(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
