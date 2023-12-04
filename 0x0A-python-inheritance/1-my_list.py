cd19f230ae888c6171d6c42046e2eaf34a33958f
archMk <victormurithi2020@gmail.com>
2023-12-04 18:59:58 +0300

    printing a sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A python class that inherits from the list class
+"""
+
+
+class MyList(list):
+    """inherits from the class list class"""
+    def __init__(self):
+        """Initializes the list class"""
+        super().__init__()
+
+    def print_sorted(self):
+        """An extension of the list class that prints
+        a sorted list of integers in ascending order
+
+        Return:
+            a sorted list from the smallest value to
+            the largest in the list
+        """
+        s_list = super().copy()
+        print(sorted(s_list))
