39b668bcaa87a74c522d9c4af1f5fe4970ef4a4a
archMk <victormurithi2020@gmail.com>
2023-12-04 17:58:29 +0300

    What's inside the class

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,9 @@
-#!/usr/python3
+#!/usr/bin/python3
 """
 Listing all the attributes of an object
 """
 
 
-def lookup(obj):
+def lookup(obj) -> list:
     """List out all the builtin attributes of an object"""
     return dir(obj)
