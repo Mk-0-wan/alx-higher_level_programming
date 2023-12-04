addc542335d53620c970027398a7f6682bccd0d4
archMk <victormurithi2020@gmail.com>
2023-12-04 21:37:05 +0300

    inherits directly or indirectly

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""indirect and direct inheritance"""
+
+
+def inherits_from(obj, a_class):
+    """checks if an object inherits direct or indirectly
+    from a_class
+    or a subclass of a_class
+
+    Args:
+        obj (object): a python object of any type
+        a_class (class): any class type for python
+    Return:
+        a bool if an object is both an instance of that
+        class or a subclass of that class
+    """
+    if obj:
+        return issubclass(obj.__class__, a_class)
