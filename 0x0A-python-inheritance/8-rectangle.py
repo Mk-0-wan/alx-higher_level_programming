350094f1e1d8aeff669084b8724ccf6815c41784
archMk <victormurithi2020@gmail.com>
2023-12-05 01:14:52 +0300

    inheritance

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,7 +1,32 @@
 #!/usr/bin/python3
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
 """simple Base Class"""
 
+
+class BaseGeometry:
+    """An empty Base class"""
+
+    def __init__(self):
+        """initializes the class instances attribute"""
+        pass
+
+    def area(self):
+        """simple area function that raises an exception error
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates if a value is an integer
+
+        Args:
+            name (str): a string value for the method validator
+            value (int): integer value for the validator
+        Return:
+            raises some exception errors
+        """
+        if type(value) is not int: # checks for the exact class
+            raise TypeError("{:s} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
 
 # no use of builtin commands
 class Rectangle(BaseGeometry):
