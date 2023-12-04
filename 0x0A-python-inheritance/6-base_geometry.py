93d4d16c09adca0c72e28a6a4fbf29d7f00f760c
archMk <victormurithi2020@gmail.com>
2023-12-04 22:03:10 +0300

    simple exception raise error

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""simple Base Class"""
+
+
+class BaseGeometry:
+    """An empty Base class"""
+
+    def __init__(self):
+        """initializes the class"""
+        pass
+
+    def area(self)
+        """simple area function that raises an
+        exception error
+        """
+        raise Exception("area() is not implemented")
