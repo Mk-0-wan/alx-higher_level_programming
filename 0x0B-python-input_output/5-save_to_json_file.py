8ac9597549fa3e2779f13abcbcf7c495da2c1faf
archMk <victormurithi2020@gmail.com>
2023-12-06 02:27:22 +0300

    removing all the typeError message

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -14,4 +14,4 @@ def save_to_json_file(my_obj, filename):
         number of chars written by the user
     """
     with open(filename, "w", encoding="utf-8") as fp:
-       return json.dump(my_obj, fp)
+        return json.dump(my_obj, fp)
