80516be035748791d2741572a9947c82928f2c70
archMk <victormurithi2020@gmail.com>
2023-12-06 01:34:37 +0300

    class instance attributes

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -10,9 +10,11 @@ class Student:
         Args:
             first_name (str): string format of students name
             last_name (str): string format of students name
+            age (int): students age
         """
         self.first_name = first_name
         self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """A dictionary representation of the class attributes"""
