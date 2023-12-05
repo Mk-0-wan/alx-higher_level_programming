a8c33a7524eb2a2ffa7b036c7f4deede63c1e6a2
archMk <victormurithi2020@gmail.com>
2023-12-05 03:36:57 +0300

    fixing errors

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -13,5 +13,4 @@ def is_kind_of_class(obj, a_class) -> bool:
         a bool if an object is both an instance of that
         class or a subclass of that class
     """
-    if obj and a_class:
-        return isinstance(obj, a_class) or issubclass(type(obj), a_class)
+    return isinstance(obj, a_class)
