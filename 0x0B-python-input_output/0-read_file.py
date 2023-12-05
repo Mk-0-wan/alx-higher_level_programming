311534ec04668b1a36ecfb5c4c4e5aa05c0fec99
archMk <victormurithi2020@gmail.com>
2023-12-05 17:34:52 +0300

    removing trailing whitespaces

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,7 +3,10 @@
 
 
 def read_file(filename=""):
-    """function that reads all the contents of a file"""
-    if filename.__class__ is str:
-        with open(filename, encoding="utf-8") as fp:
-            print(fp.read().strip())
+    """function that reads all the contents of a file
+
+   Args:
+        filename (str):name of the file to write to
+    """
+    with open(filename, encoding="utf-8") as fp:
+        print(fp.read().strip())
