ad7b39b5dee7892b7f648cc98f29a613127cee29
archMk <victormurithi2020@gmail.com>
2023-12-05 19:00:23 +0300

    dumping json to a file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Writing to a json file"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Writing to a file and returns the number char written
+
+   Args:
+        filename (str):name of the file to write to
+        my_obj (str): input data to be written which is a python
+        string object
+    Return:
+        number of chars written by the user
+    """
+    try:
+        with open(filename, "w", encoding="utf-8") as fp:
+            json.dump(my_obj, fp)
+    except TypeError:
+        raise TypeError("{} is not JSON serializable".format(my_obj))
