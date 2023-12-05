0ac2bcdd46131aaa8a34652a1d1a929dd9a6ef31
archMk <victormurithi2020@gmail.com>
2023-12-06 02:24:31 +0300

    printing errors

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -12,6 +12,6 @@ def to_json_string(my_obj):
     Return:
          a json formatted data from python string object
     """
-    if not isinstance(my_obj, (int, float, list, dict, tuple, str, bool)):
-        raise TypeError("{:s} is not JSON serializable".format(my_obj))
+    #if not isinstance(my_obj, (int, float, list, dict, tuple, str, bool)):
+        #raise TypeError("{:s} is not JSON serializable".format(my_obj))
     return (json.dumps(my_obj))
