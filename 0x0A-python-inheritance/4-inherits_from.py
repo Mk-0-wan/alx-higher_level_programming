0cbff2b99ddbaffa5ecbdb4bbb032e5d6957297c
archMk <victormurithi2020@gmail.com>
2023-12-05 03:54:49 +0300

    type() excplicit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -14,4 +14,4 @@ def inherits_from(obj, a_class):
         a bool if an object is both an instance of that
         class or a subclass of that class
     """
-    return issubclass(obj.__class__, a_class) or not isinstance(obj, a_class)
+    return issubclass(obj.__class__, a_class) and type(obj) is not a_class
