90324ed9056b69e2b7545bbf7076d134c06bd87d
archMk <victormurithi2020@gmail.com>
2023-12-06 00:56:13 +0300

    reading and writting to a json file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,14 +6,17 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
-try:
-    lst = load_from_json_file("add_item.json")
-except:
-    lst = []
+def read_write_to_json():
+    """simple python function that writes to a file"""
+    try:
+        lst = load_from_json_file("add_item.json")
+    except TypeError:
+        lst = []
 
-lst.extend([sys.argv[i] for i in range(len(sys.argv)) if i != 0])
+    lst.extend([sys.argv[i] for i in range(len(sys.argv)) if i != 0])
 
-try:
     save_to_json_file(lst, "add_item.json")
-except:
-    pass
+
+
+if __name__ == "__main__":
+    read_write_to_json()
