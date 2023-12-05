b72d70bfbac044abd00b6cef7555d034c23e403c
archMk <victormurithi2020@gmail.com>
2023-12-05 18:18:56 +0300

    json objects

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Json format"""
+import json
+
+
+def to_json_string(my_obj):
+    """Performs a serialization process
+
+    Args:
+        my_obj (str): a python string object
+
+    Return:
+         a json formatted data from python string object
+    """
+    if not isinstance(my_obj, (int, list, dict, tuple, str)):
+        raise TypeError("{} is not a JSON serializable".format(my_obj))
+    return (json.dumps(my_obj))
