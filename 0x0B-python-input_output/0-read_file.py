30f3c4451c33a0168adca5e4d341459817b13e55
archMk <victormurithi2020@gmail.com>
2023-12-06 02:19:10 +0300

    fixed some error

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,7 +6,7 @@ def read_file(filename=""):
     """function that reads all the contents of a file
 
    Args:
-        filename (str):name of the file to write to
+        filename (str):name of the file to read to
     """
     with open(filename, encoding="utf-8") as fp:
-        print(fp.read().strip())
+        print(fp.read(), end='')
