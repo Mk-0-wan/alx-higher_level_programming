a741dd3ca1a909720b67a06fd114d79b2c2b6893
archMk <victormurithi2020@gmail.com>
2023-12-05 17:04:11 +0300

    reading contents of a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Reading from a file"""
+
+
+def read_file(filename=""):
+    """function that reads all the contents of a file"""
+    with open(filename, encoding="utf-8") as fp:
+        print(fp.read())
