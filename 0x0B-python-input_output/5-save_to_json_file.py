973a02d2aeed4879d9cf8e4abf215c12b8e76a11
archMk <victormurithi2020@gmail.com>
2023-12-06 00:34:24 +0300

    saving to a json file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -15,7 +15,6 @@ def save_to_json_file(my_obj, filename):
     """
     try:
         with open(filename, "w", encoding="utf-8") as fp:
-            json.dump(my_obj, fp)
-            fp.write("\n")
+            return json.dump(my_obj, fp)
     except TypeError:
         raise TypeError("{} is not JSON serializable".format(my_obj))
