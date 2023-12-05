34f3c441e59bf63b1b453c114ddbd22726ea644e
archMk <victormurithi2020@gmail.com>
2023-12-05 18:34:57 +0300

    adding boolen values

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -12,6 +12,6 @@ def to_json_string(my_obj):
     Return:
          a json formatted data from python string object
     """
-    if not isinstance(my_obj, (int, list, dict, tuple, str)):
+    if not isinstance(my_obj, (int, float, list, dict, tuple, str, bool)):
         raise TypeError("{} is not JSON serializable".format(my_obj))
     return (json.dumps(my_obj))
