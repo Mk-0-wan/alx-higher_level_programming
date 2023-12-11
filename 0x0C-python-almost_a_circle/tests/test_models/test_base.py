2819118ce29f1915595dc5f91377cbc9f5209749
archMk <victormurithi2020@gmail.com>
2023-12-11 20:32:58 +0300

    first commit

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 """Simple unittest module"""
 import unittest
-
+from models.base import Base
 
 class TestId(unittest.TestCase):
     """simple test unit for id"""
 
     def test_id(self):
         """Test if id is an integer"""
-        x_int = int(32)
-        self.assertEqual(x_int, int(12))
+        b1 = Base()
+        self.assertEqual(b1.id, int(32))
+        b1 = Base(32)
+        self.assertEqual(b1.id, int(32))
+    def test_char_id(self):
+        """Test for a char id value"""
+        b1 = Base()
+        b1 = Base('a')
+        self.assertRaises()
 
 if __name__ == "__main__":
     unittest.main()
