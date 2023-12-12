348c50cea6debe2c6ebe7a72d4baa89a74c78302
archMk <victormurithi2020@gmail.com>
2023-12-12 04:30:49 +0300

    adding test case for the base class

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Simple unittest module"""
 import unittest
+import pep8
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
@@ -27,3 +28,29 @@ class TestId(unittest.TestCase):
 
         b1 = Base()
         self.assertEqual(b1.id, int(5))
+
+    def test_pep8_base(self):
+        """Test that the base module conforms to PEP8."""
+        style = pep8.StyleGuide()
+        result = style.check_files(['models/base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_cls_doc(self):
+        """Test class doc """
+
+        self.assertFalse(len(Base.__doc__) == 0)
+
+    def test_init(self):
+        """Test __init__"""
+
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        b4 = Base(12)
+        b5 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
+        self.assertEqual(b4.id, 12)
+        self.assertEqual(b5.id, 4)
