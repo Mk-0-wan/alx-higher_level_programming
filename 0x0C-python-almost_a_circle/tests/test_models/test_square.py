31d9dde23c6421ae33959e3e2b25ee258bd62701
archMk <victormurithi2020@gmail.com>
2023-12-12 05:15:09 +0300

    square test cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -3,7 +3,6 @@
 from models.square import Square
 import unittest
 import io
-import unittest
 from unittest.mock import patch
 
 class TestRectangle(unittest.TestCase):
@@ -15,28 +14,27 @@ class TestRectangle(unittest.TestCase):
 
     def test_init(self):
         """Test for the initialized attributes"""
-        s1 = Square(5)
+        s1 = Square(6)
         self.assertEqual(s1.size, 6)
         self.assertEqual(s1.width, 6)
         self.assertEqual(s1.height, 6)
 
+    def test_disply(self):
+        """Test for how to display"""
+
+        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            s1 = Square(3)
+            s1.display()
+            self.assertEqual(mock_stdout.getvalue(),
+                             '###\n###\n###\n')
 
     def test_update(self):
-        """Test for checking the update function"""
+        """Test for the update function"""
 
         s1 = Square(5)
 
-        with patch('sys.stdout', new_callable=io.StringIO) as mock:
+        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
             s1.update(10)
             print(s1)
-            self.assertEqual(mock.getvalue(),
+            self.assertEqual(mock_stdout.getvalue(),
                              '[Square] (10) 0/0 - 5\n')
-
-    def test_disply(self):
-        """Test for checking the display"""
-
-        with patch('sys.stdout', new_callable=io.StringIO) as mock:
-            s1 = Square(4)
-            s1.display()
-            self.assertEqual(mock.getvalue(),
-                             '####\n####\n####\n####\n')
