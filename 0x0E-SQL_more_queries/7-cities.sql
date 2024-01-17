7775ef0f80cf3e9b3d755cff262beacb0aeca8f0
tmk_ubuntu <ubuntu0mk@gmail.com>
2024-01-18 02:36:59 +0300

    look up for the other user

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,6 @@
 -- Create a database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 -- Create table from the database created above
 CREATE TABLE IF NOT EXISTS cities (
 	id INT PRIMARY KEY AUTO_INCREMENT,
