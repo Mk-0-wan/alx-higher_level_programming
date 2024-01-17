e012ed7596d4671f300381564046d86cc0c07b42
tmk_ubuntu <ubuntu0mk@gmail.com>
2024-01-18 02:05:31 +0300

    look up for the other user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Granting all permission to the new created USER
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
