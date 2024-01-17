b4e019d64c5839cb0b3ae05c9d7ed4384a88cc63
tmk_ubuntu <ubuntu0mk@gmail.com>
2024-01-18 02:35:13 +0300

    look up for the other user

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,8 @@
 -- Create a database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 -- Create table from the database created above
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS states(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
        	name VARCHAR(256) NOT NULL
 );
