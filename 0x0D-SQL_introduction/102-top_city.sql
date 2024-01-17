0cb26447374b5870f4ad4ddbaff46a357312d276
archMk <victormurithi2020@gmail.com>
2024-01-17 04:05:33 +0300

    finding average of max temp values

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- top three cities between july and agust
-SELECT city, AVG(MAX(value)) AS avg_temp FROM temparetures WHERE month BETWEEN 7 AND 8 GROUP BY city ORDER BY avg_temp DESC;
+SELECT city, AVG(MAX(value)) AS avg_temp FROM temperatures WHERE month BETWEEN 7 AND 8 GROUP BY city ORDER BY avg_temp DESC;
