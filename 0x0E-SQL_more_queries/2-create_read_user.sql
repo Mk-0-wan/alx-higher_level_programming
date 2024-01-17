0272f2d8877f9ca6d10e673e5531c2f734284924
tmk_ubuntu <ubuntu0mk@gmail.com>
2024-01-18 02:19:44 +0300

    look up for the other user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,4 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Creating a new user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Adding only SELECT privileges for the user_0d_1
-GRANT SELECT ON hbtn_0d_2.* 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
