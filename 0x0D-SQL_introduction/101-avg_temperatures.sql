c86c2c8ee8f882c5744aef51a9c8e19a70071d7b
archMk <victormurithi2020@gmail.com>
2024-01-17 03:45:00 +0300

    importing from another file

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- Sourcing Database
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY value DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
