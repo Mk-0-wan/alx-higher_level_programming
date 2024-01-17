2c76590e50cb018e9d3f987f2a867e4aedff1f29
archMk <victormurithi2020@gmail.com>
2024-01-17 03:25:12 +0300

    migrating to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,3 @@
 -- Moving all your database to utf8 encoding
-DROP DATABASE IF EXISTS hbtn_0c_0;
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0 CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE hbtn_0c_0;
-CREATE TABLE IF NOT EXISTS first_table ( id INT(11), name VARCHAR(256) );
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
