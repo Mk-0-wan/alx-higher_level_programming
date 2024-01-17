569d148fd3f0e0a89230c9c3ae7f6caebea33e66
Victor Mk <114664618+Mk-0-wan@users.noreply.github.com>
2024-01-17 11:38:10 +0300

    Update 102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- top three cities between july and agust
-SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY AVG(value) DESC) results LIMIT 3;
+SELECT city, avg_temp FROM ( SELECT city, AVG(value) as avg_temp FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY AVG(value) DESC) results LIMIT 3;
