903a6fea94b537f479de562a29fad47fd614f2e0
tmk_ubuntu <ubuntu0mk@gmail.com>
2024-01-18 18:13:58 +0300

    Join everything from the left table to the right table

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- Using the left join option -- joins all the rows in the left table to the right table
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
