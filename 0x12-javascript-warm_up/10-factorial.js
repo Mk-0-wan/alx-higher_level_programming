4ae7a54881c56ad0843c971cabfb13992a6f82a1
mk <mk@mail.com>
2024-02-13 18:38:21 +0300

    Fixing js linter issues

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,15 +3,14 @@
 // check if num is a Number
 const num = process.argv[2];
 if (isNaN(num)) {
-	return 1;
+  return 1;
 }
 // perform the factorial algorithm
 function factorial(n) {
-	if (n === 0)  {
-		return 1;
-	} else {
-		return n * factorial(n - 1);
-	}
+  if (n === 0)  {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
-
 console.log(factorial(parseInt(num)));
