15566da04fa3ab094286d5ffc2d5704981819534
mk <mk@mail.com>
2024-02-13 18:25:37 +0300

    added the negative integer implementation

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 if (process.argv[2]) {
   const myVar = process.argv[2];
-  if (/^\d+$/.test(myVar) || /^\d+\.\d+$/.test(myVar)) {
+  if (/^[-]?\d+$/.test(myVar) || /^[-]?\d+\.\d+$/.test(myVar)) {
     console.log(`My number: ${myVar}`);
   } else {
     console.log('Not a number');
