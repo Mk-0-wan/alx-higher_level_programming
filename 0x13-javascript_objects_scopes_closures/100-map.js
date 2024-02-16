f079982f84be0f93952e6200cc545e4e6a59270b
mk <mk@mail.com>
2024-02-16 12:18:22 +0300

    Multiplying elements with their index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,7 +2,7 @@
 
 const list = require('./100-data').list;
 
-const newList = list.map((x, index) => { return x * index});
+const newList = list.map((x, index) => { return x * index; });
 
 console.log(list);
 console.log(newList);
