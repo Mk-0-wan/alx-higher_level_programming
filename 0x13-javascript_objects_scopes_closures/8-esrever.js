e60867c4966c017852704757aab4dc5b0fd22476
mk <mk@mail.com>
2024-02-16 06:31:38 +0300

    Fix01: fixing javascript linter

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-/*Getting the reverse order of a string */
-exports.esrever = function(list)  {
-	const lastIndex = (list.length - 1);
-	let emptyList = [];
+/* Getting the reverse order of a string */
+exports.esrever = function (list) {
+  const lastIndex = (list.length - 1);
+  const emptyList = [];
 
-	for (let i = lastIndex; i >= 0; i--) {
-		emptyList.push(list[i]);
-	}
-	console.log(emptyList);
-}
+  for (let i = lastIndex; i >= 0; i--) {
+    emptyList.push(list[i]);
+  }
+  console.log(emptyList);
+};
