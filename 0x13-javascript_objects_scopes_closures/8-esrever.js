0290164553bc183368a9fc1350a2c8ab42e3cec6
mk <mk@mail.com>
2024-02-16 08:48:00 +0300

    Fix02:Updated the function to return instead of printing

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 /* Getting the reverse order of a string */
 exports.esrever = function (list) {
-  const lastIndex = (list.length - 1);
   const emptyList = [];
+  const lastIndex = (list.length - 1);
 
   for (let i = lastIndex; i >= 0; i--) {
     emptyList.push(list[i]);
   }
-  console.log(emptyList);
+  return (emptyList);
 };
