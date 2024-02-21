be83f2d0dcc6f02bd17a7879fe9ad431153f2195
owan-mk <owan-ubuntu@gmail.com>
2024-02-21 23:30:29 +0300

    Using where clause with the equal sign

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -17,37 +17,22 @@ if __name__ == '__main__':
     # make a list of all the queries to try
     # include the left join right join full and inner join
 
-    select_queirs = [
-        "SELECT cities.id, cities.name, states.name FROM cities INNER JOIN\
-         states ON cities.state_id = states.id WHERE BINARY states.name LIKE\
-         ORDER BY cities.id ASC",
-
-        "SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN\
-         states ON cities.state_id = states.id WHERE BINARY states.name LIKE\
-         ORDER BY cities.id ASC",
-
-        "SELECT cities.id, cities.name, states.name FROM cities RIGHT JOIN\
-         states ON cities.state_id = states.id WHERE BINARY states.name LIKE\
-         ORDER BY cities.id ASC",
-    ]
+    select_queirs = "SELECT cities.id, cities.name, states.name\
+         FROM cities INNER JOIN states ON cities.state_id = states.id\
+         WHERE states.name = %s ORDER BY cities.id ASC"
 
     # with block will automatically close both the database and the cursor
     # once the with block is exited
     try:
         with mdb.connect(**database_config) as db:
             with db.cursor() as cur:
-                for query in select_queries:
-                    try:
-                        cur.execute(query, ('%' + sys.argv[4] + '%',))
-                        cur_rows = cur.fetchall()
-                        if (cur_rows):
-                            for indx, row in enumerate(cur_rows):
-                                print(row[1], end=("\n", ",")
-                                      [(indx + 1) < len(cur_rows)])
-                        else:
-                            print()
-                        break
-                    except mdb.Error as e:
-                        continue
+                cur.execute(query, (sys.argv[4],))
+                cur_rows = cur.fetchall()
+                if (cur_rows):
+                    for indx, row in enumerate(cur_rows):
+                        print(row[1],
+                              end=("\n", ",")[(indx + 1) < len(cur_rows)])
+                else:
+                    print()
     except mdb.Error as e:
         raise (e)
