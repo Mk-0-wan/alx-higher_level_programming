1ea741fa52369c6e26d7014cce486a02ddb1e576
owan-mk <owan-ubuntu@gmail.com>
2024-02-23 02:14:31 +0300

    state relationship to citie

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Translating a python class object and mapping it to a
+ relational mysql database"""
+from sqlalchemy import String, Integer, Column
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Class state which inherits from the Base(declerative_base) in order
+    to make a simple connection to the database in order to make the table from
+    this class instance
+
+    Args:
+        __tablename__ (class  instance attribute): holds the table name
+        id(integer): this will be translated to the id column in the sql table
+        name(string): this will be translated to the name column in the sql
+    """
+    __tablename__ = 'states'
+
+    # setting up the id properties
+    id = Column(
+        'id',
+        Integer,
+        primary_key=True,
+        autoincrement=True,
+        nullable=False,
+        unique=True
+    )
+
+    # setting up the name properties
+    name = Column(
+        'name',
+        String(128),
+        nullable=False
+    )
+
+    related_cities = relationship(
+        'City',
+        cascade="all, delete",
+        backref='state'
+    )
