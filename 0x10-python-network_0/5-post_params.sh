bb74394b84ef2c5f662c520199f9929935b0274b
mk <victormurithi2020@gmail.com>
2024-03-01 01:12:48 +0300

    Removed the Post method

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sending a post request using curl
-curl -s -d 'email=test%40gmail%2ecom&subject=I%20will%20always%20be%20here%20for%20PLD' -X POST "$1"
+curl -s -d 'email=test%40gmail%2ecom&subject=I%20will%20always%20be%20here%20for%20PLD' "$1"
