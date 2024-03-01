80810a3c03a67740d5fe2049039ece60af8a1631
mk <victormurithi2020@gmail.com>
2024-03-01 04:10:12 +0300

    Doesn't make sense yet but soon it will

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# getting the body of request that returned 200 status response sh -c "[ $(curl -s -o /dev/null -w '%{http_code}' "$1") -eq '200' ]" &&
-echo -n "$(curl -s --get "$1")"
+# getting the body of request that returned 200 status response
+curl -s -L "$1"
