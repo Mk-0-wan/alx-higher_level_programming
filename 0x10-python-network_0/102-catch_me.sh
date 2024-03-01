2f58cbfa830525649dfa0640f40dcae5dca2d5ed
mk <victormurithi2020@gmail.com>
2024-03-01 05:09:26 +0300

    Found you

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # getting the request from another location 
-curl -s -L -X PUT -d "user_id=98" -H "Origin: school" 0.0.0.0:5000/catch_me_2
+curl -s -L -X PUT -d "user_id=98" -H "Origin: school" 0.0.0.0:5000/catch_me
