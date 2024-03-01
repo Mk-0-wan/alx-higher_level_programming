4cb552005cd20ece650c33e6c885759f7d2e77b0
mk <victormurithi2020@gmail.com>
2024-03-02 01:33:38 +0300

    Getting specific headers

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Getting specific headers from the response using requests"""
+if __name__ == "__main__":
+    from requests import get
+    from sys import argv
+
+    response = get(argv[1])
+    print(response.headers['X-Request-Id'])
