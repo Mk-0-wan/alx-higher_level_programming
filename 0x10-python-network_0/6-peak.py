779790c005c94140e8847fb5d25fe62c2d2d5a0e
mk <victormurithi2020@gmail.com>
2024-03-01 16:48:58 +0300

    Finally used BST

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,22 +2,55 @@
 """Simple implementation of the Bubble sort algorithm"""
 
 
-def find_peak(list_of_integers):
-    """Finds the peak in a list of integers"""
-    if (len(list_of_integers) == 0):
-        return
-    # first we need a max value
-    max_val = max(list_of_integers)
-    # loop over each of the element comparing each one of them to each other
-    i = 0
-    # 1 ,2 ,4, 6  == len == 3 - 2 = 1 loop has two turn to make
-    while (i < (len(list_of_integers) - 2)):
-        if (list_of_integers[i] > list_of_integers[i + 1]):
-            if (list_of_integers[i] > max_val):
-                max_val = list_of_integers[i]
-        else:
-            if (list_of_integers[i + 1] > max_val):
-                max_val = list_of_integers[i + 1]
-        i += 1
+class BinarySearchTree():
+    """Making a simple binary search tree"""
 
-    return max_val
+    def __init__(self, root):
+        """Initialize the class with the a value"""
+        self.left = None
+        self.right = None
+        self.root = root
+
+    def insert_node(self, value):
+        """Inserting new value to the BST"""
+        # check if value is less than root value
+        if self.root > value:
+            if self.left is None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert_node(value)
+        else:
+            if self.right is None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert_node(value)
+
+    def in_order_traversal(self):
+        """Printing the first left node then right node"""
+        if self.left:
+            self.left.in_order_traversal()
+        print(self.root)
+        if self.right:
+            self.right.in_order_traversal()
+
+    def get_the_rightmost_node(self):
+        """Finding the rightmost node"""
+        current = self
+        while (current.right):
+            current = current.right
+        return current
+
+
+def find_peak(list_of_integers):
+    """"Finding the peak value using BST"""
+    if len(list_of_integers) == 0:
+        return None
+
+    bst = BinarySearchTree(0)
+
+    for i in list_of_integers:
+        bst.insert_node(i)
+
+    peak_val = bst.get_the_rightmost_node()
+
+    return (peak_val.root)
