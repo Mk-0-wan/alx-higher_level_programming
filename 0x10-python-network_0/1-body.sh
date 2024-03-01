d9153281403f8062189beb4ce571355fd32fbf1b
mk <victormurithi2020@gmail.com>
2024-03-01 03:00:52 +0300

    removed the silent option and deleted its output

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # getting the body of request that returned 200 status response
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s -w "%{stdout}" "$1" | tr -d "\n"
+curl -s -o /dev/null -w "%{http_code}" "$1" | grep -o "200" > /dev/null && curl -s -w "%{stdout}" "$1" | tr -d "\n"
