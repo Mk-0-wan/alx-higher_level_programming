2c33e3d5640b5f85e93790ed1872493790fcce03
mk <victormurithi2020@gmail.com>
2024-03-01 03:28:59 +0300

    Implemented a new idea

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # getting the body of request that returned 200 status response
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -o "200" > /dev/null && curl -s -w "%{stdout}" "$1" | tr -d "\n"
+sh -c "[ $(curl -s -o /dev/null -w '%{http_code}' "$1") -eq '200' ]" && curl -s --get "$1"
