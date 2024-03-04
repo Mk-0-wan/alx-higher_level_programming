2b02feecfb8ece7ae9544f398e781d2adff50d35
mkish <victormurithi2020@gmail.com>
2024-03-05 01:12:32 +0300

    All commits in one go

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
 
     response = get(url, git_params, headers=git_headers)
     if (response.status_code != 200):
+        print('None')
         exit()
 
     commits = response.json()
