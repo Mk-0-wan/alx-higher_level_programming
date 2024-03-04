83077fa392e10bdd6af480907e72eea88c5281b4
mk <victormurithi2020@gmail.com>
2024-03-04 07:54:38 +0300

    Working with GitHubRest API

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,12 +5,13 @@ if __name__ == "__main__":
     import requests
     from sys import argv, exit
 
+    url = f'https://api.github.com/users/{argv[1]}'
     custom_header = {
             'Accept': 'application/vnd.github+json',
             'Authorization': f'Bearer {argv[2]}',
             'X-GitHub-Api-Version': '2022-11-28',
             }
-    response = requests.get(f'https://api.github.com/users/{argv[1]}', headers=custom_header)
+    response = requests.get(url, headers=custom_header)
     if (response.status_code != 200):
         print("None")
         exit()
