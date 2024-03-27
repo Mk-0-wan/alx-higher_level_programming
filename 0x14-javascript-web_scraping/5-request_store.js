8bef5abe557799284e6a7ec05dd33873bfadeecf
mk <victormurithi2020@gmail.com>
2024-03-27 05:29:22 +0300

    scrapping the web

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -8,8 +8,6 @@ const fileName = process.argv[3];
 request(URL, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const newObject = body;
-    console.log(newObject);
-
     fs.writeFile(fileName, newObject, { encoding: 'utf-8' }, err => {
       if (err) { console.log(err); }
     });
