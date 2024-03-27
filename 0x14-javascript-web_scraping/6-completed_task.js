8d0296fce0e909954f0c4078a9873ec2a77ec5f6
mk <victormurithi2020@gmail.com>
2024-03-27 07:02:59 +0300

    grouping tasked done together

--- 0x14-javascript-web_scraping/6-completed_task.js
+++ 0x14-javascript-web_scraping/6-completed_task.js
@@ -1,23 +1,22 @@
 #!/usr/bin/node
 
 const request = require('request');
-
 const url = process.argv[2];
+const taskTable = {};
+const arr = [];
 
-request(url, function (error, response, body) {
+request(url, (error, response, body) => {
   if (!error && response.statusCode === 200) {
-    let results = {};
-    let json = JSON.parse(body);
+    const json = JSON.parse(body);
     for (let i = 0; i < json.length; i++) {
-      let task = json[i];
-      if (task.completed === true) {
-        if (results?.task?.userId !== undefined) {
-          results?.task?.userId += 1;
-        } else {
-          results?.task?.userId = 1;
-        }
+      if (json?.[i]?.completed === true) {
+        const counts = json?.[i]?.userId;
+        arr.push(counts);
       }
     }
-    console.log(results);
+    arr.forEach(element => {
+      taskTable[element] = (taskTable[element] || 0) + 1;
+    });
+    console.log(taskTable);
   }
 });
