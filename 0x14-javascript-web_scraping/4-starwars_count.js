5c642f355c63f1821e78ff266d1d971f70b95ddc
mk <victormurithi2020@gmail.com>
2024-03-27 05:02:12 +0300

    getting user id 18:

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const { count } = require('console');
+const request = require('request');
+const URL = process.argv[2];
+
+request(URL, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const newObject = JSON.parse(body);
+    goingUp = newObject.results;
+    let count = 0;
+    for(let i = 0; i < newObject.count; i++) {
+      let p = goingUp?.[i]?.characters;
+      for(let j = 0; j < p.length; j++) {
+        if (p[j] === "https://swapi-api.alx-tools.com/api/people/18/") {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
