49a3ec706612a3a5a16eebe70a69288a7b8ceec8
mk <victormurithi2020@gmail.com>
2024-03-27 05:07:08 +0300

    getting user id 18:

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,6 +2,7 @@
 
 const request = require('request');
 const URL = process.argv[2];
+const userId = 18;
 
 request(URL, (error, response, body) => {
   if (!error && response.statusCode === 200) {
@@ -11,7 +12,7 @@ request(URL, (error, response, body) => {
     for (let i = 0; i < newObject.count; i++) {
       const p = goingUp?.[i]?.characters;
       for (let j = 0; j < p.length; j++) {
-        if (p[j] === 'https://swapi-api.alx-tools.com/api/people/18/') {
+        if (p[j] === `https://swapi-api.alx-tools.com/api/people/${userId}/`) {
           count++;
         }
       }
